fa65ba37a21750244045f1c4b6511b0836c16822
yashethra <airishyashethra@gmail.com>
2025-08-09 13:09:17 +0530

    Game.js

---
+++ Game.js
@@ -0,0 +1,11 @@
+const sounds = {
+  playerHit: new Audio('https://raw.githubusercontent.com/northernstar001-ctrl/northernstar001-ctrl.github.io/main/Player%20hit.mp3'),
+  bgMusic: new Audio('https://raw.githubusercontent.com/northernstar001-ctrl/northernstar001-ctrl.github.io/main/Background%20music.mp3'),
+  explosion: new Audio('https://raw.githubusercontent.com/northernstar001-ctrl/northernstar001-ctrl.github.io/main/explosion.mp3'),
+  enemyHit: new Audio('https://raw.githubusercontent.com/northernstar001-ctrl/northernstar001-ctrl.github.io/main/Enemy%20hit.mp3'),
+  bossHit: new Audio('https://raw.githubusercontent.com/northernstar001-ctrl/northernstar001-ctrl.github.io/main/Boss%20hit.mp3'),
+  reload: new Audio('https://raw.githubusercontent.com/northernstar001-ctrl/northernstar001-ctrl.github.io/main/Reload.mp3'),
+  pistol: new Audio('https://raw.githubusercontent.com/northernstar001-ctrl/northernstar001-ctrl.github.io/main/pistol.mp3'),
+  shotgun: new Audio('https://raw.githubusercontent.com/northernstar001-ctrl/northernstar001-ctrl.github.io/main/shotgun.mp3'),
+  refire: new Audio('https://raw.githubusercontent.com/northernstar001-ctrl/northernstar001-ctrl.github.io/main/Refile.mp3'),
+};
